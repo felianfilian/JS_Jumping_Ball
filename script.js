b86cd37e51c649b7a66b7d74ff1abc8d9378588a
Dennis Schmalstieg <darianbellentor@outlook.de>
2021-12-27 16:14:49 +0100

    Drawing logic

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 window.onload = function () {
 
+    var canvas = document.getElementById("canvas");
+    var ctx = canvas.getContext("2d");
 
+    startGame();
+
+    function startGame() {
+        draw();
+    }
+
+    function draw() {
+
+        ctx.rect(100, 100, 100, 100);
+        ctx.stroke();
+    }
 
 
 }
