6e116bd5231fec860057f2e8a16e1be5e0c06964
Dennis Schmalstieg <darianbellentor@outlook.de>
2021-12-28 11:35:31 +0100

    control pad added

--- script.js
+++ script.js
@@ -13,6 +13,16 @@ window.onload = function () {
 
         ctx.rect(100, 100, 100, 100);
         ctx.stroke();
+        /// title text
+        ctx.beginPath();
+        ctx.font = "30px Arial";
+        ctx.fillStyle = "#e0deff";
+        ctx.fillText("Jumping Ball", 20, 50);
+
+        /// ball
+        ctx.beginPath();
+        ctx.arc(300, 350, 50, 0, 2 * Math.PI);
+        ctx.stroke();
     }
 
 
