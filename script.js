f1140103e3d17e8795674e7b7974a93be34a8274
Dennis Schmalstieg <darianbellentor@outlook.de>
2021-12-28 12:13:57 +0100

    input logic v01

--- script.js
+++ script.js
@@ -3,10 +3,18 @@ window.onload = function () {
     var canvas = document.getElementById("canvas");
     var ctx = canvas.getContext("2d");
 
+    var dir = 0;
+    var ballX = 300;
+    var ballY = 350;
+
+    getInput();
     startGame();
 
+    setInterval(debug01, 100);
+
     function startGame() {
-        draw();
+        setInterval(draw, 32);
+        //draw();
     }
 
     function draw() {
@@ -21,9 +29,44 @@ window.onload = function () {
 
         /// ball
         ctx.beginPath();
-        ctx.arc(300, 350, 50, 0, 2 * Math.PI);
+        ctx.arc(ballX, ballY, 50, 0, 2 * Math.PI);
         ctx.stroke();
+
     }
 
+    function getInput() {
+        // input buttons
+        let btnUp = document.getElementById("up");
+        let btnLeft = document.getElementById("left");
+        let btnDown = document.getElementById("down");
+        let btnRight = document.getElementById("right");
+
+        btnUp.onmousedown = function () { dir = 4; }
+        btnDown.onmousedown = function () { dir = 3; }
+        btnLeft.onmousedown = function () { dir = 2; }
+        btnRight.onmousedown = function () { dir = 1; }
+
+        btnUp.ontouchstart = function () { dir = 4; }
+        btnDown.ontouchstart = function () { dir = 3; }
+        btnLeft.ontouchstart = function () { dir = 2; }
+        btnRight.ontouchstart = function () { dir = 1; }
+
+        btnUp.onmouseup = function () { dir = 0; }
+        btnDown.onmouseup = function () { dir = 0; }
+        btnLeft.onmouseup = function () { dir = 0; }
+        btnRight.onmouseup = function () { dir = 0; }
+
+        btnUp.ontouchend = function () { dir = 0; }
+        btnDown.ontouchend = function () { dir = 0; }
+        btnLeft.ontouchend = function () { dir = 0; }
+        btnRight.ontouchend = function () { dir = 0; }
+
+        if (dir == 1) { ballX += 50; }
+
+    }
+
+    function debug01() {
+        console.log(dir);
+    }
 
 }
